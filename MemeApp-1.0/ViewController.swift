88e97ce4318ff6451f20ff5f3590630844beb693
Felipe <fe.valdiviaa@gmail.com>
2017-11-13 20:08:53 -0600

    feature: Project creation
    Basic structure for MemeApp version 1.0.

---
+++ MemeApp-1.0/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MemeApp-1.0
+//
+//  Created by Felipe Valdivia on 11/13/17.
+//  Copyright © 2017 Felipe Valdivia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
