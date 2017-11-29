3bbdc335f41027abbdad1fc3bc180aaabed00914
Felipe <fe.valdiviaa@gmail.com>
2017-11-28 21:19:34 -0600

    Feature: Add function to hide keyboard
    Add function to hide keyboard and add function to clean text from text
    field add text field delegate to the controller view.

--- MemeApp-1.0/ViewController.swift
+++ MemeApp-1.0/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextFieldDelegate {
     // MARK: IBOutlets
     @IBOutlet weak var imageContent: UIImageView!
     @IBOutlet weak var cameraButton: UIButton!
@@ -25,22 +25,47 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         // Setting up the text for the textfields
         topText.text = "TOP"
         bottomText.text = "BOTTOM"
-        // set text alignment
-        topText.textAlignment = .center
-        bottomText.textAlignment = .center
+        
         // setting attributes
         let memeTextAttributes:[String:Any] = [
             NSStrokeColorAttributeName: UIColor.black,
             NSForegroundColorAttributeName: UIColor.white,
             NSFontAttributeName: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
-            NSStrokeWidthAttributeName: 2.0 ]
+            NSStrokeWidthAttributeName: -4.0 ]
         
         // setting the attributes to the textfields
         topText.defaultTextAttributes = memeTextAttributes
         bottomText.defaultTextAttributes = memeTextAttributes
+        
+        // set text alignment
+        topText.textAlignment = .center
+        bottomText.textAlignment = .center
+        
+        // delegate for textField
+        topText.delegate = self
+        bottomText.delegate = self
     }
     
-
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        subscribeToKeyboardNotifications()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        unsubscribeFromKeyboardNotifications()
+    }
+    
+    // MARK: Function to clean text
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        if textField.text == "TOP" {
+            topText.text = ""
+        } else if textField.text == "BOTTOM" {
+            bottomText.text = ""
+        }
+    }
+    
+    // MARK: Function to launch photo library
     @IBAction func pickImage(_ sender: Any) {
         let chooseImage = UIImagePickerController()
         chooseImage.delegate = self
@@ -48,6 +73,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         self.present(chooseImage, animated: true, completion: nil)
     }
     
+    // MARK: Function to take a picture
     @IBAction func takePicture(_ sender: Any) {
         let pictureTaken = UIImagePickerController()
         pictureTaken.delegate = self
@@ -55,7 +81,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         self.present(pictureTaken, animated: true, completion: nil)
     }
     
-    
+    // MARK: Function to pick and assing a photo from library
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]){
         
         if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
@@ -63,10 +89,38 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             dismiss(animated: true, completion: nil)
         }
     }
+
     
-    func imagePickerControllerDidCancel(_ picker: UIImagePickerController){
-        
+    func keyboardWillShow(_ notification:Notification) {
+        if bottomText.isFirstResponder {
+            view.frame.origin.y = 0 - getKeyboardHeight(notification)
+        }
     }
+    
+    func keyboardWillHide(_ notification: Notification) {
+        if bottomText.isFirstResponder {
+            view.frame.origin.y = 0
+        }
+    }
+    
+    func getKeyboardHeight(_ notification:Notification) -> CGFloat {
+        
+        let userInfo = notification.userInfo
+        let keyboardSize = userInfo![UIKeyboardFrameEndUserInfoKey] as! NSValue // of CGRect
+        return keyboardSize.cgRectValue.height
+    }
+    
+    
+    func subscribeToKeyboardNotifications() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: .UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: .UIKeyboardWillHide, object: nil)
+    }
+    
+    func unsubscribeFromKeyboardNotifications() {
+        NotificationCenter.default.removeObserver(self, name: .UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.removeObserver(self, name: .UIKeyboardWillHide, object: nil)
+    }
+    
 
 }
 
